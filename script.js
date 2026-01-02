1270c24e470abac4d9d5823044a3995fa8dac645
Autumn Williams <63067844+AutumnHW@users.noreply.github.com>
2026-01-01 21:34:20 -0500

    new website lol

---
+++ script.js
@@ -0,0 +1,4 @@
+//var backgroundURL = 0;
+//var backgroundID = Math.floor(Math.random() * 7);
+//backgroundURL = "url(images/deepintechlockscreen.jpg)";
+//document.getElementById('background').style.backgroundImage = backgroundURL;
